3966b285ca51c51c2148cc949a0030e41bb1cfca
toppy007 <33899845+toppy007@users.noreply.github.com>
2023-06-27 14:28:59 +0100

    updated files

--- CV_Optimisation_Program/job_advert/test_job_advert.py
+++
@@ -1,6 +0,0 @@
-from job_advert.job_advert import JobAdvert
-
-def test_job_ad_scraper():
-    response = JobAdvert.job_ad_scrapper('https://app.otta.com/jobs/NzNpVWNM')
-
-    assert response is not None
